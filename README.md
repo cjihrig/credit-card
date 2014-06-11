2ac0149c6f5bc8a59a84273aea825fddd82ecb97
cjihrig <cjihrig@gmail.com>
2014-06-10 22:54:07 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,204 @@
-credit-card
-===========
+# credit-card
 
-Credit card validation
+Module for performing credit card validation.
+
+## Basic Usage
+
+Pass credit card information to the `validate()` method. In the following example, the credit card information is stored in `card`.
+
+```javascript
+var CreditCard = require('credit-card');
+var card = {
+  cardType: 'VISA',
+  number: '4111111111111111',
+  expiryMonth: '03',
+  expiryYear: '2100',
+  cvv: '123'
+};
+var validation = CreditCard.validate(card);
+```
+
+The `validation` object returned by `validate()` will look like this:
+
+```javascript
+{
+  card: {
+    cardType: 'VISA',
+    number: '4111111111111111',
+    expiryMonth: '03',
+    expiryYear: '2100',
+    cvv: '123'
+  },
+  validCardNumber: true,
+  validExpiryMonth: true,
+  validExpiryYear: true,
+  validCvv: true,
+  isExpired: false
+}
+```
+
+## Methods
+
+### `validate(card [, options])`
+
+  - Arguments
+    - `card` (object) - An object containing credit card information. Should be of the following format:
+        - `cardType` (string) - One of the supported card types.
+        - `number` (string) - Credit card number.
+        - `expiryMonth` (string or number) - Credit card expiration month.
+        - `expiryYear` (string or number) - Credit card expiration year.
+        - `cvv` (string) - Credit card CVV code.
+    - `options` (object) - An optional object used to define custom card types and default values. Can define the following fields:
+        - `cardTypes` (object) - An object used to validate credit card types. This allows new types, such as custom gift cards, to be defined.
+        - `expiryMonths` (object) - An object used to override default values related to expiry months.
+        - `expiryYears` (object) - An object used to override default values related to expiry years.
+  - Returns
+    - object - An object containing the input card data and the results of validation. Should adhere to the following schema:
+        - `card` (object) - This holds the value of the `card` argument.
+        - `validCardNumber` (boolean) - The result of calling `isValidCardNumber()`.
+        - `validExpiryMonth` (boolean) - The result of calling `isValidExpiryMonth()`.
+        - `validExpiryYear` (boolean) - The result of calling `isValidExpiryYear()`.
+        - `validCvv` (boolean) - The result of calling `doesCvvMatchType()`.
+        - `isExpired` (boolean) - The result of calling `isExpired()`.
+        - `customValidation` (any) - The result of custom validation. Can be any data type.
+
+Performs several validation checks on credit card data. 
+
+### `isValidCardNumber(number, type [, options])`
+
+  - Arguments
+    - `number` (string) - Credit card number string.
+    - `type` (string) - Credit card type.
+    - `options` (object) - An optional object used to define additional card types.
+  - Returns
+    - boolean - `true` if the number is valid for the credit card type and passes the Luhn algorithm, `false` otherwise.
+
+Determines if a credit card number is valid for a given credit card type. Also verifies that the credit card number passes the Luhn algorithm.
+
+### `isValidExpiryMonth(month [, options])`
+
+  - Arguments
+    - `month` (number or string) - Month value to be evaluated.
+    - `options` (object) - An optional object used to define the minimum and maximum month values accepted.
+
+Determines if a value is a valid credit card expiry month. The month must fall between the defined minimum and maximum months. This range is 1 to 12 by default, but can be adjusted using the `options` input.
+
+### `isValidExpiryYear(year [, options])`
+
+  - Arguments
+    - `year` (number or string) - Year value to be evaluated.
+    - `options` (object) - An optional object used to define the minimum and maximum year values accepted.
+
+Determines if a value is a valid credit card expiry year. The year must fall between the defined minimum and maximum years. This range is 1900 to 2200 by default, but can be adjusted using the `options` input.
+
+### `doesNumberMatchType(number, type [, options])`
+
+  - Arguments
+    - `number` (string) - Credit card number string.
+    - `type` (string) - Credit card type.
+    - `options` (object) - An optional object used to define additional card types.
+  - Returns
+    - boolean - `true` if the number is valid for the credit card type, `false` otherwise.
+
+Determines if a credit card number is valid for a given credit card type.
+
+### `doesCvvMatchType(number, type [, options])`
+
+  - Arguments
+    - `number` (string) - CVV string.
+    - `type` (string) - Credit card type.
+    - `options` (object) - An optional object used to define additional card types.
+  - Returns
+    - boolean - `true` if the CVV is valid for the credit card type, `false` otherwise.
+
+Determines if a CVV is valid for a given credit card type. For example, American Express requires a four digit CVV, while Visa and Mastercard require a three digit CVV.
+
+### `isExpired(month, year)`
+
+  - Arguments
+    - `month` (number) - Expiry month of card.
+    - `year` (number) - Expiry year of card.
+  - Returns
+    - boolean - `true` if the expiration date has been reached, `false` otherwise.
+
+Determines if a credit card's expiration date has been reached.
+
+### `luhn(number)`
+
+  - Arguments
+    - `number` (string) - The number string to check.
+  - Returns
+    - boolean - `true` if the number passes, `false` otherwise.
+
+Executes the [Luhn algorithm](http://en.wikipedia.org/wiki/Luhn_algorithm) on a string to determine if it is a valid credit card number.
+
+### `sanitizeNumberString(number)`
+
+  - Arguments
+    - `number` (string) - The number string to be sanitized.
+  - Returns
+    - string - The sanitized string.
+
+Strips all non-numeric characters from `number`. If `number` is not a string, an empty string is returned.
+
+## Supported Credit Card Types
+
+This module supports a variety of credit cards. To better accommodate a wider range of clients, the card types have been aliased to other names as well.
+
+  - `VISA` - Aliased to `vc`, `VC`, and `visa`.
+  - `MASTERCARD` - Aliased to `mc`, `MC`, `mastercard`, `master card`, and `MASTER CARD`.
+  - `AMERICANEXPRESS` - Aliased to `ae`, `AE`, `ax`, `AX`, `amex`, `AMEX`, `american express`, and `AMERICAN EXPRESS`.
+  - `DINERSCLUB` - Aliased to `dinersclub`.
+  - `DISCOVER` - Aliased to `dc`, `DC`, and `discover`.
+  - `JCB` - Aliased to `jcb`.
+
+## Defining Custom Card Types
+
+The following example defines and validates a new card type known as `GIFT_CARD`. Notice that the `card` variable has its `cardType` set to `GIFT_CARD`, and a new `pin` field has been defined. The `options` variable is passed as the second argument to `validate()`. These options define the new `GIFT_CARD` type. The `cardPattern` is a regular expression that all gift card numbers should match. Similarly, `cvvPattern` is a regular expression that the CVV should match.
+
+Also notice the `customValidation` function. This function is used when normal validation is not quite enough. This function is passed the `card` object and `settings` object used by `validate()`. This function can return any data, which will be added directly to the response.
+
+```javascript
+var CreditCard = require('credit-card');
+var card = {
+  cardType: 'GIFT_CARD',
+  number: '4111111111111111',
+  expiryMonth: '03',
+  expiryYear: '2100',
+  pin: '7890'
+};
+var options = {
+  cardTypes: {
+    GIFT_CARD: {
+      cardPattern: /^4[0-9]{12}(?:[0-9]{3})?$/,
+      cvvPattern: /.*/
+    }
+  },
+  customValidation: function(card, settings) {
+    if (card.cardType === 'GIFT_CARD') {
+      return card.pin === '7890';
+    }
+  }
+};
+var validation = CreditCard.validate(card, options);
+```
+
+The value of `validation` is shown below.
+
+```javascript
+{
+  card: {
+    cardType: 'GIFT_CARD',
+    number: '4111111111111111',
+    expiryMonth: '03',
+    expiryYear: '2100',
+    pin: '7890'
+  },
+  validCardNumber: true,
+  validExpiryMonth: true,
+  validExpiryYear: true,
+  validCvv: true,
+  isExpired: false,
+  customValidation: true
+}
+```
---
+++ test/index.js
@@ -0,0 +1,409 @@
+var Lab = require('lab');
+var CreditCard = require('../');
+
+var expect = Lab.expect;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+// a list of test credit cards is available at:
+// http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+
+
+describe('#validate()', function() {
+  it('no invalid responses on valid card', function(done) {
+    var card = {
+      cardType: 'VISA',
+      number: '4111111111111111',
+      expiryMonth: '03',
+      expiryYear: '2100',
+      cvv: '123'
+    };
+    var validation = CreditCard.validate(card);
+
+    expect(validation.card).to.deep.equal(card);
+    expect(validation.validCardNumber).to.equal(true);
+    expect(validation.validExpiryMonth).to.equal(true);
+    expect(validation.validExpiryYear).to.equal(true);
+    expect(validation.validCvv).to.equal(true);
+    expect(validation.isExpired).to.equal(false);
+    expect(validation.customValidation).to.not.exist;
+    done();
+  });
+
+  it('no invalid responses on valid card by alias', function(done) {
+    var card = {
+      cardType: 'VC',
+      number: '4111111111111111',
+      expiryMonth: '03',
+      expiryYear: '2100',
+      cvv: '123'
+    };
+    var validation = CreditCard.validate(card);
+
+    expect(validation.card).to.deep.equal(card);
+    expect(validation.validCardNumber).to.equal(true);
+    expect(validation.validExpiryMonth).to.equal(true);
+    expect(validation.validExpiryYear).to.equal(true);
+    expect(validation.validCvv).to.equal(true);
+    expect(validation.isExpired).to.equal(false);
+    expect(validation.customValidation).to.not.exist;
+    done();
+  });
+
+  it('invalid responses on invalid card', function(done) {
+    var card = {
+      cardType: 'VISA',
+      number: '4111111111111112',
+      expiryMonth: '00',
+      expiryYear: '2100',
+      cvv: '123'
+    };
+    var validation = CreditCard.validate(card);
+
+    expect(validation.card).to.deep.equal(card);
+    expect(validation.validCardNumber).to.equal(false);
+    expect(validation.validExpiryMonth).to.equal(false);
+    expect(validation.validExpiryYear).to.equal(true);
+    expect(validation.validCvv).to.equal(true);
+    expect(validation.isExpired).to.equal(false);
+    expect(validation.customValidation).to.not.exist;
+    done();
+  });
+
+  it('invalid responses on no card', function(done) {
+    var validation = CreditCard.validate();
+
+    expect(validation.card).to.deep.equal({});
+    expect(validation.validCardNumber).to.equal(false);
+    expect(validation.validExpiryMonth).to.equal(false);
+    expect(validation.validExpiryYear).to.equal(false);
+    expect(validation.validCvv).to.equal(false);
+    expect(validation.isExpired).to.equal(true);
+    expect(validation.customValidation).to.not.exist;
+    done();
+  });
+
+  it('provides custom validation function', function(done) {
+    var card = {
+      cardType: 'VISA',
+      number: '4111111111111111',
+      expiryMonth: '03',
+      expiryYear: '2100',
+      cvv: '123',
+      pin: '7890'
+    };
+    var options = {
+      customValidation: function(card, settings) {
+        return card.pin === '7890';
+      }
+    };
+    var validation = CreditCard.validate(card, options);
+
+    expect(validation.card).to.deep.equal(card);
+    expect(validation.validCardNumber).to.equal(true);
+    expect(validation.validExpiryMonth).to.equal(true);
+    expect(validation.validExpiryYear).to.equal(true);
+    expect(validation.validCvv).to.equal(true);
+    expect(validation.isExpired).to.equal(false);
+    expect(validation.customValidation).to.equal(true);
+    done();
+  });
+
+  it('defines a new card type', function(done) {
+    var card1 = {
+      cardType: 'VISA',
+      number: '4111111111111111',
+      expiryMonth: '03',
+      expiryYear: '2100',
+      cvv: '123'
+    };
+    var card2 = {
+      cardType: 'GIFT_CARD',
+      number: '4111111111111111',
+      expiryMonth: '03',
+      expiryYear: '2100',
+      pin: '7890'
+    };
+    var options = {
+      cardTypes: {
+        GIFT_CARD: {
+          cardPattern: /^4[0-9]{12}(?:[0-9]{3})?$/,
+          cvvPattern: /.*/
+        }
+      },
+      customValidation: function(card, settings) {
+        if (card.cardType === 'GIFT_CARD') {
+          return card.pin === '7890';
+        }
+      }
+    };
+    var validation1 = CreditCard.validate(card1, options);
+    var validation2 = CreditCard.validate(card2, options);
+
+    // verify that existing validation still works
+    expect(validation1.card).to.deep.equal(card1);
+    expect(validation1.validCardNumber).to.equal(true);
+    expect(validation1.validExpiryMonth).to.equal(true);
+    expect(validation1.validExpiryYear).to.equal(true);
+    expect(validation1.validCvv).to.equal(true);
+    expect(validation1.isExpired).to.equal(false);
+    expect(validation1.customValidation).to.not.exist;
+
+    // verify that custom type validation works
+    expect(validation2.card).to.deep.equal(card2);
+    expect(validation2.validCardNumber).to.equal(true);
+    expect(validation2.validExpiryMonth).to.equal(true);
+    expect(validation2.validExpiryYear).to.equal(true);
+    expect(validation2.validCvv).to.equal(true);
+    expect(validation2.isExpired).to.equal(false);
+    expect(validation2.customValidation).to.equal(true);
+
+    done();
+  });
+});
+
+
+describe('#isValidCardNumber()', function() {
+  it('returns true for valid cards', function(done) {
+    expect(CreditCard.isValidCardNumber('378282246310005', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('371449635398431', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('378734493671000', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('30569309025904', 'DINERSCLUB')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('38520000023237', 'DINERSCLUB')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('6011111111111117', 'DISCOVER')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('6011000990139424', 'DISCOVER')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('3530111333300000', 'JCB')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('3566002020360505', 'JCB')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('5555555555554444', 'MASTERCARD')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('5105105105105100', 'MASTERCARD')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('4111111111111111', 'VISA')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('4012888888881881', 'VISA')).to.equal(true);
+    expect(CreditCard.isValidCardNumber('4222222222222', 'VISA')).to.equal(true);
+    done();
+  });
+
+  it('returns false for numbers that pass luhn but fail are invalid', function(done) {
+    expect(CreditCard.isValidCardNumber('123', 'AMERICANEXPRESS')).to.equal(false);
+    done();
+  });
+});
+
+
+describe('#doesNumberMatchType()', function() {
+  it('returns true for valid card matches', function(done) {
+    expect(CreditCard.doesNumberMatchType('378282246310005', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('371449635398431', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('378734493671000', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('30569309025904', 'DINERSCLUB')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('38520000023237', 'DINERSCLUB')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('6011111111111117', 'DISCOVER')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('6011000990139424', 'DISCOVER')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('3530111333300000', 'JCB')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('3566002020360505', 'JCB')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('5555555555554444', 'MASTERCARD')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('5105105105105100', 'MASTERCARD')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('4111111111111111', 'VISA')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('4012888888881881', 'VISA')).to.equal(true);
+    expect(CreditCard.doesNumberMatchType('4222222222222', 'VISA')).to.equal(true);
+    done();
+  });
+
+  it('returns false for invalid cards', function(done) {
+    expect(CreditCard.doesNumberMatchType('4111111111111111', 'AMERICANEXPRESS')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('5555555555554444', 'DINERSCLUB')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('3530111333300000', 'DISCOVER')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('6011111111111117', 'JCB')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('30569309025904', 'MASTERCARD')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('378282246310005', 'VISA')).to.equal(false);
+    done();
+  });
+
+  it('returns false for unknown card types', function(done) {
+    expect(CreditCard.doesNumberMatchType('4111111111111111', '')).to.equal(false);
+    expect(CreditCard.doesNumberMatchType('378282246310005', 'foo')).to.equal(false);
+    done();
+  });
+
+  it('returns true for custom card types', function(done) {
+    expect(CreditCard.doesNumberMatchType('911', 'foo', {
+      foo: {
+        cardPattern: /^91*$/,
+        cvvPattern: /.*/,
+      }
+    })).to.equal(true);
+    done();
+  });
+});
+
+
+describe('#doesCvvMatchType()', function() {
+  it('returns true for valid cvv matches', function(done) {
+    expect(CreditCard.doesCvvMatchType('1234', 'AMERICANEXPRESS')).to.equal(true);
+    expect(CreditCard.doesCvvMatchType('123', 'DINERSCLUB')).to.equal(true);
+    expect(CreditCard.doesCvvMatchType('456', 'DISCOVER')).to.equal(true);
+    expect(CreditCard.doesCvvMatchType('789', 'JCB')).to.equal(true);
+    expect(CreditCard.doesCvvMatchType('012', 'MASTERCARD')).to.equal(true);
+    expect(CreditCard.doesCvvMatchType('333', 'VISA')).to.equal(true);
+    done();
+  });
+
+  it('returns false for invalid cvvs', function(done) {
+    expect(CreditCard.doesCvvMatchType('123', 'AMERICANEXPRESS')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType('1234', 'DINERSCLUB')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType('1', 'DISCOVER')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType('', 'JCB')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType(null, 'MASTERCARD')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType({}, 'VISA')).to.equal(false);
+    done();
+  });
+
+  it('returns false for unknown card types', function(done) {
+    expect(CreditCard.doesCvvMatchType('999', '')).to.equal(false);
+    expect(CreditCard.doesCvvMatchType('x', 'foo')).to.equal(false);
+    done();
+  });
+
+  it('returns true for custom card types', function(done) {
+    expect(CreditCard.doesCvvMatchType('abc', 'foo', {
+      foo: {
+        cardPattern: /.*/,
+        cvvPattern: /^[a-c]{3}$/
+      }
+    })).to.equal(true);
+    done();
+  });
+});
+
+
+describe('#isValidExpiryMonth()', function() {
+  it('returns true for valid month', function(done) {
+    expect(CreditCard.isValidExpiryMonth('01')).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth('02')).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth('3')).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(4)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(5)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(6)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(7)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(8)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(9)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth('10')).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(11)).to.equal(true);
+    expect(CreditCard.isValidExpiryMonth(12)).to.equal(true);
+    done();
+  });
+
+  it('returns false for invalid month', function(done) {
+    expect(CreditCard.isValidExpiryMonth('001')).to.equal(false);
+    expect(CreditCard.isValidExpiryMonth(0)).to.equal(false);
+    expect(CreditCard.isValidExpiryMonth(13)).to.equal(false);
+    done();
+  });
+
+  it('returns true for month in specified range', function(done) {
+    var valid = CreditCard.isValidExpiryMonth(13, {
+      min: 13,
+      max: 13
+    });
+
+    expect(valid).to.equal(true);
+    done();
+  });
+});
+
+
+describe('#isValidExpiryYear()', function() {
+  it('returns true for valid year', function(done) {
+    expect(CreditCard.isValidExpiryYear('2014')).to.equal(true);
+    expect(CreditCard.isValidExpiryYear(1990)).to.equal(true);
+    expect(CreditCard.isValidExpiryYear(1991)).to.equal(true);
+    expect(CreditCard.isValidExpiryYear(2199)).to.equal(true);
+    expect(CreditCard.isValidExpiryYear(2200)).to.equal(true);
+    done();
+  });
+
+  it('returns false for invalid year', function(done) {
+    expect(CreditCard.isValidExpiryYear('100')).to.equal(false);
+    expect(CreditCard.isValidExpiryYear(1899)).to.equal(false);
+    expect(CreditCard.isValidExpiryYear(2201)).to.equal(false);
+    done();
+  });
+
+  it('returns true for year in specified range', function(done) {
+    var valid = CreditCard.isValidExpiryYear(1800, {
+      min: 1800,
+      max: 1800
+    });
+
+    expect(valid).to.equal(true);
+    done();
+  });
+});
+
+
+describe('#isExpired()', function() {
+  it('returns true for an expired card', function(done) {
+    expect(CreditCard.isExpired(12, 2013)).to.equal(true);
+    done();
+  });
+
+  it('returns false for non-expired card', function(done) {
+    expect(CreditCard.isExpired(1, 2100)).to.equal(false);
+    done();
+  });
+});
+
+
+describe('#luhn()', function() {
+  it('returns true for valid cards', function(done) {
+    expect(CreditCard.luhn('378282246310005')).to.equal(true);
+    expect(CreditCard.luhn('371449635398431')).to.equal(true);
+    expect(CreditCard.luhn('378734493671000')).to.equal(true);
+    expect(CreditCard.luhn('5610591081018250')).to.equal(true);
+    expect(CreditCard.luhn('30569309025904')).to.equal(true);
+    expect(CreditCard.luhn('38520000023237')).to.equal(true);
+    expect(CreditCard.luhn('6011111111111117')).to.equal(true);
+    expect(CreditCard.luhn('6011000990139424')).to.equal(true);
+    expect(CreditCard.luhn('3530111333300000')).to.equal(true);
+    expect(CreditCard.luhn('3566002020360505')).to.equal(true);
+    expect(CreditCard.luhn('5555555555554444')).to.equal(true);
+    expect(CreditCard.luhn('5105105105105100')).to.equal(true);
+    expect(CreditCard.luhn('4111111111111111')).to.equal(true);
+    expect(CreditCard.luhn('4012888888881881')).to.equal(true);
+    expect(CreditCard.luhn('4222222222222')).to.equal(true);
+    expect(CreditCard.luhn('5019717010103742')).to.equal(true);
+    expect(CreditCard.luhn('6331101999990016')).to.equal(true);
+    done();
+  });
+
+  it('returns false for invalid cards', function(done) {
+    expect(CreditCard.luhn('5105105105105101')).to.equal(false);
+    expect(CreditCard.luhn('4111111111111112')).to.equal(false);
+    expect(CreditCard.luhn('')).to.equal(false);
+    expect(CreditCard.luhn(' ')).to.equal(false);
+    expect(CreditCard.luhn(undefined)).to.equal(false);
+    expect(CreditCard.luhn([])).to.equal(false);
+    expect(CreditCard.luhn('abc')).to.equal(false);
+    done();
+  });
+});
+
+
+describe('#sanitizeNumberString()', function() {
+  it('returns a string stripped of all non-numeric characters', function(done) {
+    var str = CreditCard.sanitizeNumberString('4111-1111-1111-1111');
+
+    expect(str).to.equal('4111111111111111');
+    done();
+  });
+
+  it('returns empty string for non-string input', function(done) {
+    expect(CreditCard.sanitizeNumberString()).to.equal('');
+    expect(CreditCard.sanitizeNumberString(undefined)).to.equal('');
+    expect(CreditCard.sanitizeNumberString(null)).to.equal('');
+    expect(CreditCard.sanitizeNumberString(false)).to.equal('');
+    expect(CreditCard.sanitizeNumberString([])).to.equal('');
+    expect(CreditCard.sanitizeNumberString({})).to.equal('');
+    done();
+  });
+});
